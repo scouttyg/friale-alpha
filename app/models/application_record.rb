ee9b7767fa2a8eccc3047f4b0abdf9796b9b2e33
Scott Goci <scottjg@gmail.com>
2025-09-19 10:03:22 -0700

    Add in some convenient scopes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,21 @@ class ApplicationRecord < ActiveRecord::Base
   include Hashid::Rails
   primary_abstract_class
 
+  scope :oldest_first, -> { order(id: :asc) }
+  scope :newest_first, -> { order(id: :desc) }
+
+  scope :created_after, ->(date_time = 1.week.ago) { where("#{table_name}.created_at > ?", date_time) }
+  scope :created_before, ->(date_time = 1.week.ago) { where("#{table_name}.created_at < ?", date_time) }
+  scope :created_between, lambda { |start_date, end_date|
+    where("#{table_name}.created_at >= :start_date AND #{table_name}.created_at < :end_date", start_date: start_date, end_date: end_date)
+  }
+
+  scope :updated_after, ->(date_time = 1.week.ago) { where("#{table_name}.updated_at > ?", date_time) }
+  scope :updated_before, ->(date_time = 1.week.ago) { where("#{table_name}.updated_at < ?", date_time) }
+  scope :updated_between, lambda { |start_date, end_date|
+    where("#{table_name}.updated_at >= :start_date AND #{table_name}.updated_at < :end_date", start_date: start_date, end_date: end_date)
+  }
+
   def self.ransackable_attributes(_auth_object = nil)
     authorizable_ransackable_attributes
   end

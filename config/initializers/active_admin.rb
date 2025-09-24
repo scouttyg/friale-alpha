b3cc22a0758d8af894fe9fb5a4afba7287ca3f8e
Scott Goci <scottjg@gmail.com>
2025-09-24 12:07:09 -0700

    Fix a bit of subscription mocking in local dev environments

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -154,7 +154,7 @@ ActiveAdmin.setup do |config|
   # config.comments_menu = false
   #
   # You can customize the comment menu:
-  # config.comments_menu = { parent: 'Admin', priority: 1 }
+  config.comments_menu = { parent: 'Admin Users', priority: 1 }
 
   # == Batch Actions
   #
